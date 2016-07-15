6a2dc4a57b7469f9d8581669b397d1c9635947e6
Joshua Rush <jdrush89@gmail.com>
2016-07-15 15:27:15 -0400

    feat(FormField): Allow inline validation

--- test/FormFieldValidationBlockSpec.jsx
+++ test/FormFieldValidationBlockSpec.jsx
@@ -25,10 +25,31 @@ describe('FormFieldValidationBlock', () => {
     expect(formFieldValidationBlock.props.children[2]).toEqual('test message');
   });
 
+  it('has the rs-validation-block class when not inline', () => {
+    renderer.render(<FormFieldValidationBlock value="test message" />);
+    const formFieldValidationBlock = renderer.getRenderOutput();
+
+    expect(formFieldValidationBlock.props.className).toEqual('rs-validation-block');
+  });
+
+  it('has the rs-validation-inline class when inline', () => {
+    renderer.render(<FormFieldValidationBlock value="test message" inline />);
+    const formFieldValidationBlock = renderer.getRenderOutput();
+
+    expect(formFieldValidationBlock.props.className).toEqual('rs-validation-inline');
+  });
+
   it('returns null if no field is passed to it', () => {
     renderer.render(<FormFieldValidationBlock />);
     const formFieldValidationBlock = renderer.getRenderOutput();
 
     expect(formFieldValidationBlock.type).toBe('noscript');
+  });
+
+  it('has a text-top vertical align style', () => {
+    renderer.render(<FormFieldValidationBlock value="test message" />);
+    const formFieldValidationBlock = renderer.getRenderOutput();
+
+    expect(formFieldValidationBlock.props.style['vertical-align']).toEqual('text-top');
   });
 });
